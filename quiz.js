644609c7836abaeeaecbd675a3355baf8d150b52
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-01-24 13:10:39 -0700

    added some variables for js  and id for html.

---
+++ quiz.js
@@ -0,0 +1,5 @@
+var startBtn = document.getElementById("startBtn");
+var time = 75;
+var time_remaining = true;
+var time_start = false;
+var countdownTimer = document.getElementById("countdownTimer");
