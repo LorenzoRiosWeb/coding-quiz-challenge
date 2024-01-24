ab9871428d7d3c44aaae055f41dbb66f5d83e1b8
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-01-24 13:11:38 -0700

    added changes to js and html

--- quiz.js
+++ quiz.js
@@ -1,5 +1,42 @@
+// Make variables for functionality of the code
 var startBtn = document.getElementById("startBtn");
 var time = 75;
 var time_remaining = true;
 var time_start = false;
 var countdownTimer = document.getElementById("countdownTimer");
+// create function for startBtn
+
+
+
+
+
+
+
+// create function for answers var
+
+
+
+
+
+
+
+
+//create function for inccorect answer time is subtracted
+
+
+
+
+
+
+
+
+//when time reaches 0 screen shows game over
+
+
+
+
+
+
+
+
+//creat local storage for highscore
