68eb0c9a38baf8d82167f2e45b55712f35222b44
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-01-24 14:46:31 -0700

    had to change an Id as I named 2 id after the same thing causing some issues in the code

--- quiz.js
+++ quiz.js
@@ -4,8 +4,27 @@ var time = 75;
 var time_remaining = true;
 var time_start = false;
 var countdownTimer = document.getElementById("countdownTimer");
-// create function for startBtn
+var homeContainer = document.getElementById("homeContainer");
+var quizContainer = document.getElementById("quizContainer");
+var questionHeading = document.getElementById("questionHeading");
+var answerA = document.getElementById("answerA");
+var answerB = document.getElementById("answerB");
+var answerC = document.getElementById("answerC");
+var answerD = document.getElementById("answerD");
+var answerResponser = document.getElementById("AnswerResponse");
+var high_scores =[];
+var output="";
+// set a score of =0 at start of game
+var score = 0;
 
+let i = 0;
+// create function for startBtn
+startBtn.addEventListener("click", function(){
+    quizContainer.style.display = "block";
+    homeContainer.style.display = "none";
+    countdownTimer.style.display = "block";
+    document.getElementById("")
+})
 
 
 
